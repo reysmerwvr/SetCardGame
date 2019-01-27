d50beb4aa0330d3846be7069af347567152e4c65
Reysmer Valle <reysmerwvr@gmail.com>
2019-01-26 20:27:08 -0500

    [In Progress] - Creating data structure for Deck and Card

--- SetCardGame/Models/Card.swift
+++ SetCardGame/Models/Card.swift
@@ -12,9 +12,49 @@ struct Card {
     
     private var uuid: String
     public var isFaceUp: Bool
+    public var symbol: Symbol?
+    public var shade: Shade?
+    public var number: Number?
+    public var color: Color?
     
-    init() {
+    enum Symbol: String {
+        case triangle = "▲"
+        case circle = "●"
+        case square = "■"
+        
+        static var all = [Symbol.triangle, Symbol.circle, Symbol.square]
+    }
+    
+    enum Shade: String {
+        case solid = "solid"
+        case striped = "striped"
+        case open = "open"
+        
+        static var all = [Shade.solid, Shade.striped, Shade.open]
+    }
+    
+    enum Number: Int {
+        case one = 1
+        case two = 2
+        case three = 3
+        
+        static var all = [Number.one, Number.two, Number.three]
+    }
+    
+    enum Color: String {
+        case red = "#colorLiteral(red: 0.2638142705, green: 0.01383866463, blue: 0, alpha: 1)"
+        case green = "#colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)"
+        case blue = "#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)"
+        
+        static var all = [Color.red, Color.green, Color.blue]
+    }
+    
+    init(symbol: Symbol, shade: Shade, number: Number, color: Color) {
         self.uuid = UUID().uuidString
         self.isFaceUp = false
+        self.symbol = symbol;
+        self.shade = shade;
+        self.number = number;
+        self.color = color;
     }
 }
--- SetCardGame/Models/Deck.swift
+++ SetCardGame/Models/Deck.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+
+struct Deck {
+    
+    private(set) var cards: [Card] = [Card]()
+    
+    init() {
+        for symbol in Card.Symbol.all {
+            for shade in Card.Shade.all {
+                for number in Card.Number.all {
+                    for color in Card.Color.all {
+                        cards.append(Card(symbol: symbol, shade: shade, number: number, color: color))
+                    }
+                }
+            }
+        }
+    }
+    
+    mutating public func draw() -> Card? {
+        if cards.count > 0 {
+            return cards.remove(at: cards.count.arc4random)
+        } else {
+            return nil
+        }
+    }
+}
+
+extension Int {
+    var arc4random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(abs(self))))
+        } else {
+            return 0
+        }
+    }
+}
